894b64de10c2274d1ff68c9bef302ad05865cb47
Your Name <you@example.com>
2025-07-11 01:37:58 -0300

    correcao .env

--- setup.sh
+++ setup.sh
@@ -12,6 +12,10 @@ INSTALL_DIR="$(pwd)"
 cp "$INSTALL_DIR/.env.template" "$INSTALL_DIR/.env"
 sed -i "s/^USER_ID=.*/USER_ID=$USER_ID/" "$INSTALL_DIR/.env"
 
+# Garante que o .env esteja em /opt/1984-deny
+sudo mkdir -p /opt/1984-deny
+sudo cp "$INSTALL_DIR/.env" /opt/1984-deny/.env
+
 # Move a pasta wsdl para /temp/wsdl, sobrescrevendo se já existir
 [ -d /temp ] || mkdir /temp
 rm -rf /temp/wsdl
