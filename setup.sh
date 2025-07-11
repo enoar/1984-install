f4a7b89c3880d6df1b1acc041eea5e441c49a3eb
Your Name <you@example.com>
2025-07-11 00:40:42 -0300

    Correcao diretorio

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@
 set -e
 echo "🛡️ Instalador automático do sistema 1984-Deny"
 read -p "Digite o USER_ID do cliente: " USER_ID
-INSTALL_DIR="/opt/1984-deny"
-git clone git@github.com:enoar/1984-install.git "$INSTALL_DIR"
+INSTALL_DIR="$(pwd)"
 cp "$INSTALL_DIR/.env.template" "$INSTALL_DIR/.env"
 sed -i "s/^USER_ID=.*/USER_ID=$USER_ID/" "$INSTALL_DIR/.env"
 
